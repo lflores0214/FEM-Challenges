d06065f00c4db23ec4ab8436a7dd15ff7d597972
Luis <lflores02142@gmail.com>
2021-05-27 13:17:36 -0700

    switched calculator logic to use a switch block instead of if statements

--- calculator-app-main/js/index.js
+++ calculator-app-main/js/index.js
@@ -2,7 +2,7 @@ const calculator = document.querySelector(".calculator");
 const keys = document.querySelectorAll(".btn");
 const display = document.getElementById("display");
 const themeToggle = document.getElementById("toggle-btn");
-const container = document.getElementById("container")
+const container = document.getElementById("container");
 
 themeToggle.addEventListener("click", (e) => {
   const themeDic = { 1: "theme1", 2: "theme2", 3: "theme3" };
@@ -13,7 +13,6 @@ const calculate = (num1, operator, num2) => {
   let answer;
   const n1 = parseFloat(num1);
   const n2 = parseFloat(num2);
-
   if (operator === "add") {
     answer = n1 + n2;
   }
@@ -38,52 +37,60 @@ keys.forEach((e) => {
     const displayedNum = display.textContent;
     const previousKeyType = calculator.dataset.previousKeyType;
 
-    if (!action) {
-      if (displayedNum === "0" || previousKeyType === "operator") {
-        display.textContent = keyContent;
+    if (displayedNum === "") {
+      display.textContent = "0";
+    }
+
+    switch (action) {
+      case "add":
+      case "subtract":
+      case "multiply":
+      case "divide":
+        calculator.dataset.num1 = displayedNum;
+        calculator.dataset.operator = action;
+        calculator.dataset.previousKeyType = "operator";
+        return;
+
+      case "decimal":
+        if (displayedNum === "0" || previousKeyType === "operator") {
+          display.textContent = "0.";
+          calculator.dataset.previousKeyType = "";
+        }
+        if (!displayedNum.includes(".")) {
+          display.textContent = displayedNum + keyContent;
+        }
+        return;
+
+      case "calculate":
+        const num1 = calculator.dataset.num1;
+        const operator = calculator.dataset.operator;
+        const num2 = displayedNum;
+        display.textContent = calculate(num1, operator, num2);
+        return;
+
+      case "reset":
+        display.textContent = 0;
+        calculator.dataset.num1 = "";
+        calculator.dataset.operator = "";
         calculator.dataset.previousKeyType = "";
-      } else {
-        display.textContent = displayedNum + keyContent;
-      }
-    }
+        return;
 
-    if (action === "decimal") {
-      if (displayedNum === "0" || previousKeyType === "operator") {
-        display.textContent = "0.";
-        calculator.dataset.previousKeyType = "";
-      }
-      if (!displayedNum.includes(".")) {
-        display.textContent = displayedNum + keyContent;
-      }
-    }
+      case "delete":
+        console.log(displayedNum.length);
+        if (displayedNum.length === 1) {
+          display.textContent = "0";
+        } else {
+          display.textContent = displayedNum.slice(0, -1);
+        }
+        return;
 
-    if (
-      action === "add" ||
-      action === "subtract" ||
-      action === "multiply" ||
-      action === "divide"
-    ) {
-      calculator.dataset.num1 = displayedNum;
-      calculator.dataset.operator = action;
-      calculator.dataset.previousKeyType = "operator";
-    }
-
-    if (action === "calculate") {
-      const num1 = calculator.dataset.num1;
-      const operator = calculator.dataset.operator;
-      const num2 = displayedNum;
-      display.textContent = calculate(num1, operator, num2);
-    }
-
-    if (action === "delete") {
-      console.log("delete");
-    }
-
-    if (action === "reset") {
-      display.textContent = 0;
-      calculator.dataset.num1 = "";
-      calculator.dataset.operator = "";
-      calculator.dataset.previousKeyType = "";
+      default:
+        if (displayedNum === "0" || previousKeyType === "operator") {
+          display.textContent = keyContent;
+          calculator.dataset.previousKeyType = "";
+        } else {
+          display.textContent = displayedNum + keyContent;
+        }
     }
   });
 });
